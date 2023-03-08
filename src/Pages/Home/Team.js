c7bfabf3fe6254b35400024c530ad11aae9560f8
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-03-08 12:16:20 +0600

    team ection updated

--- src/Pages/Home/Team.js
+++ src/Pages/Home/Team.js
@@ -8,12 +8,12 @@ import { Link } from 'react-router-dom'
 import { FaFacebookF, FaInstagram,  FaLinkedinIn } from 'react-icons/fa'
 function Team() {
   return (
-    <div className='lg:p-24' style={{backgroundImage:(`url(${dot})`)}} >
-      <div className='max-w-screen-xl mx-auto min-h-screen' >
+    <div className='lg:p-24 min-h-screen' style={{backgroundImage:(`url(${dot})`)}} >
+      <div className='max-w-screen-xl mx-auto ' >
       <div className='relative m-auto pb-5 px-5' >
-       <h2 className="font-bold text-6xl text-start relative top-[70px]  md:text-[120px] text-gray-300 opacity-[0.07] numbs uppercase hidden lg:block"> creative TEAM</h2>
-       <h2 className="font-bold text-6xl text-start relative top-[70px]  md:text-[120px] text-gray-300 opacity-[0.07] numbs uppercase lg:hidden block"> TEAM</h2>
-        <h2 data-aos="zoom-in-right" className='uppercase   font-extrabold mb-5 text-transparent lg:text-4xl md:text-3xl font-serif  lg:leading-relaxed tracking-wider lg:tracking-[2px] bg-clip-text bg-gradient-to-r from-[#b18551] to-[#000]'>About Our creative Team Spirit.</h2>
+       <h2 className="font-bold text-6xl text-start   md:text-[120px] text-gray-300 opacity-[0.07] numbs uppercase hidden lg:block"> creative TEAM</h2>
+       <h2 className="font-bold text-6xl text-start   md:text-[120px] text-gray-300 opacity-[0.07] numbs uppercase lg:hidden block"> TEAM</h2>
+        <h2 data-aos="zoom-in-right" className='uppercase relative bottom-[70px]  font-extrabold mb-5 text-transparent lg:text-4xl md:text-3xl font-serif  lg:leading-relaxed tracking-wider lg:tracking-[2px] bg-clip-text bg-gradient-to-r from-[#b18551] to-[#000]'>About Our creative Team Spirit.</h2>
         {/* <p>You've got to invest in the world, you've got to read, you've got to go to art galleries, you've got to find out the names of plants. You've got to start to love the world and know about the whole genius of the human race. We're amazing people.</p> */}
        </div>
         <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-5 px-5' data-aos="fade-up">
