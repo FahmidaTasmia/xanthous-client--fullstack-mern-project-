5dab0a31a75134036ad7933b80c88ef2c95000d9
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-04-10 03:25:00 +0600

    login signup updated

--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -1,41 +1,41 @@
-import React, { useContext, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useForm } from "react-hook-form";
-import { FaGoogle } from "react-icons/fa";
-import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react'
+import beg from '../../../asset/beg.jpg'
 import { AuthContext } from '../../../contexts/AuthProvider';
-import logo from '../../../asset/Log.png'
-
-
-const Login = () => {
-
-
-    //form
+import { GoogleAuthProvider } from 'firebase/auth';
+import useToken from './../../../hooks/useToken';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+function Login() {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const { signIn,providerLogin } = useContext(AuthContext);
     const googleProvider = new GoogleAuthProvider();
     const [loginError, setLoginError] = useState('');
+    const [loginUserEmail, setLoginUserEmail] = useState('');
+    const [token] = useToken(loginUserEmail);
     const location = useLocation();
     const navigate = useNavigate();
 
     const from = location.state?.from?.pathname || '/';
 
-  
+    if (token) {
+        navigate(from, { replace: true });
+    }
 
     const handleLogin = data => {
-      console.log(data);
-      setLoginError('');
-      signIn(data.email, data.password)
-          .then(result => {
-              const user = result.user;
-              console.log(user);
-              navigate(from, {replace: true});
-          })
-          .catch(error => {
-              console.log(error.message)
-              setLoginError(error.message);
-          });
-  }
+        console.log(data);
+        setLoginError('');
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setLoginUserEmail(data.email);
+            })
+            .catch(error => {
+                console.log(error.message)
+                setLoginError(error.message);
+            });
+    }
 
     //google
     const handleGoogleSignIn=()=>{
@@ -47,56 +47,46 @@ const Login = () => {
       .catch(error=>console.error(error));
 
   }
-    return (
-       <div className=''>
-         <div className='hero w-full my-20 ' >
-            <div className='hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row'>
-
-            <div className="w-[100vh]">
-             <img src={logo} alt="" />
-            </div>
-            <div className='w-96 p-7'>
-                <div className='grid justify-items-center'>
-                <h2>LOG IN</h2>
-                </div>
-               
-                <form onSubmit={handleSubmit(handleLogin)}>
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Email</span></label>
+  return (
+    <div className=' relative lg:pt-24 py-8 min-h-screen' style={{backgroundImage:(`url(${beg})`), backgroundSize:'cover', }}  >
+       <div className="absolute inset-0  bg-[#020B0D] opacity-30"></div>
+       <div className='p-12 lg:p-8'></div>
+      <div className='lg:w-[50%] md:w-[60%] grid content-center m-auto border-double border-[5px] border-accent md:p-10 w-[90%]' data-aos="zoom-in-up">
+      <div className='text-white  scale-75 text-xl '>
+       <form  onSubmit={handleSubmit(handleLogin)}  >
+                    <div className="form-control  ">
+                        <label className="label"> <span className="label-text text-white text-xl">Email</span></label>
                         <input type="text"
                             {...register("email", {
                                 required: "Email Address is required"
                             })}
-                            className="input input-bordered w-full max-w-xs" />
+                            className="input input-bordered w-full rounded-none border-primary bg-transparent" />
                         {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Password</span></label>
+                    <div className="form-control w-full rounded-none">
+                        <label className="label"> <span className="label-text text-white text-xl">Password</span></label>
                         <input type="password"
                             {...register("password", {
                                 required: "Password is required",
                                 minLength: { value: 6, message: 'Password must be 6 characters or longer' }
                             })}
-                            className="input input-bordered w-full max-w-xs" />
-                        <label className="label"> <span className="label-text">Forget Password?</span></label>
+                            className="input input-bordered w-full rounded-none border-primary bg-transparent" />
+                        <label className="label"> <span className="label-text  text-xl text-primary">Forget Password?</span></label>
                         {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                     </div>
-                    <input className='btn btn-primary text-white font-bold w-full' value="Login" type="submit" />
+                    <input className='btn btn-primary text-white  w-full rounded-none border-primary text-xl tracking-wider' value="Login" type="submit" />
                     <div>
                         {loginError && <p className='text-red-600'>{loginError}</p>}
                     </div>
                 </form>
-                <p>New to Cmart ?<Link className='text-secondary' to="/register">Please <span className='text-primary font-bold'>Create new Account</span></Link></p>
+                <p className='mt-2'>New to Xanthous ? <Link className='text-secondary' to="/register">Please <span className='text-primary font-bold'>Create new Account</span></Link></p>
                 <div className="divider">OR</div>
-                <button className='btn btn-outline w-full hover:bg-primary hover:border-none gap-2' onClick={handleGoogleSignIn}><FaGoogle></FaGoogle>CONTINUE WITH GOOGLE</button>
-
-              
-            </div>
-            </div>
-           
-        </div>
+                <button className='btn btn-outline w-full rounded-none border-primary text-xl tracking-wider text-white hover:bg-primary hover:border-none gap-2' onClick={handleGoogleSignIn}><FaGoogle></FaGoogle>CONTINUE WITH GOOGLE</button>
        </div>
-    );
-};
+      </div>
+      <div className='p-5 lg:p-12'></div>
+    </div>
+  )
+}
 
-export default Login;
+export default Login
--- src/Pages/Shared/Login/Register.js
+++ src/Pages/Shared/Login/Register.js
@@ -1,35 +1,43 @@
-import { GoogleAuthProvider } from 'firebase/auth';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { AuthContext } from './../../../contexts/AuthProvider';
+import { AuthContext } from '../../../contexts/AuthProvider';
+import { GoogleAuthProvider } from 'firebase/auth';
+import useToken from '../../../hooks/useToken';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import beg from '../../../asset/beg.jpg'
 import { FaGoogle } from 'react-icons/fa';
-import logo from '../../../asset/Log.png'
-
-const Register = () => {
+function Register() {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const googleProvider = new GoogleAuthProvider();
     const { createUser, updateUser,providerLogin } = useContext(AuthContext);
-    const [signUpError, setSignUPError] = useState('')
-    const location = useLocation();
+    const googleProvider = new GoogleAuthProvider();
+    const [signUpError, setSignUPError] = useState('');
+    const [createdUserEmail, setCreatedUserEmail] = useState('')
+    const [token] = useToken(createdUserEmail);
     const navigate = useNavigate();
 
-    const from = location.state?.from?.pathname || '/';
+   useEffect(()=>{
+     if(token){
+        navigate('/');
+    }
+   },[navigate, token])
+
     const handleSignUp = (data) => {
+        
         console.log(data);
         setSignUPError('');
         createUser(data.email, data.password)
             .then(result => {
-                const user = result.user;
-                console.log(user);
-                navigate(from, {replace: true});
+                // const user = result.user;
+                // // console.log(user);
                 toast('User Created Successfully.')
                 const userInfo = {
                     displayName: data.name
                 }
                 updateUser(userInfo)
-                    .then(() => { })
+                    .then(() => {
+                        saveUser(data.name, data.email, data.select);
+                    })
                     .catch(err => console.log(err));
             })
             .catch(error => {
@@ -38,63 +46,89 @@ const Register = () => {
             });
     }
 
-    const handleGoogleSignIn=()=>{
-      providerLogin(googleProvider)
-      .then(result=>{
-          const user = result.user;
-          console.log(user);
-      })
-      .catch(error=>console.error(error));
+    const saveUser = (name, email, role) =>{
+   
+        const user ={name, email,role};
+        console.log(user);
+        fetch('https://secondhand-seller-server.vercel.app/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+            setCreatedUserEmail(email);
+        })
+    }
 
-  }
+      //google
+      const handleGoogleSignIn=()=>{
+        providerLogin(googleProvider)
+        .then(result=>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error=>console.error(error));
+  
+    }
 
-    return (
-     <div className='min-h-screen max-w-screen-xl mx-auto'>
-       <div className='grid grid-cols-2 '>
-       <div className="w-[100vh]">
-             <img src={logo} alt="" />
-            </div>
-        {/* //form */}
-
-        <div className=' flex justify-center items-center'>
-            <div className='w-96 p-7'>
-                <h2 className='text-xl text-center'>Sign Up</h2>
-                <form onSubmit={handleSubmit(handleSignUp)}>
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Name</span></label>
+  return (
+    <div className=' relative lg:pt-24 py-8 min-h-screen' style={{backgroundImage:(`url(${beg})`), backgroundSize:'cover', }}  >
+       <div className="absolute inset-0  bg-[#020B0D] opacity-30"></div>
+       <div className='p-5 md:p-16 lg:p-8'></div>
+      <div className='lg:w-[50%] md:w-[60%] grid content-center m-auto border-double border-[5px] border-accent md:p-10 w-[90%]' data-aos="zoom-in-up">
+      <div className='text-white  scale-75 text-xl '>
+      <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full bg-transparent border-primary rounded-none">
+                        <label className="label"> <span className="label-text text-xl text-white">Name</span></label>
                         <input type="text" {...register("name", {
                             required: "Name is Required"
-                        })} className="input input-bordered w-full max-w-xs" />
+                        })} className="input input-bordered w-full bg-transparent border-primary rounded-none" />
                         {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
                     </div>
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Email</span></label>
+                    <div className="form-control w-full bg-transparent border-primary rounded-none">
+                        <label className="label"> <span className="label-text text-xl text-white">Email</span></label>
                         <input type="email" {...register("email", {
                             required: true
-                        })} className="input input-bordered w-full max-w-xs" />
+                        })} className="input input-bordered w-full bg-transparent border-primary rounded-none" />
                         {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                     </div>
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Password</span></label>
+                    <div className="form-control w-full bg-transparent border-primary rounded-none">
+                        <label className="label"> <span className="label-text text-xl text-white">Password</span></label>
                         <input type="password" {...register("password", {
                             required: "Password is required",
                             minLength: { value: 6, message: "Password must be 6 characters long" },
                             pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
-                        })} className="input input-bordered w-full max-w-xs" />
+                        })} className="input input-bordered w-full bg-transparent border-primary rounded-none" />
                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                     </div>
-                    <input className='btn btn-primary w-full mt-4 text-white' value="Sign Up" type="submit" />
+
+                    <div className='form-control grid self-center mt-5'>
+                    <label className='bg-transparent' >before submit select who you are!</label>
+                    <select {...register("select", { required: "required*" })} className="select select-bordered select-sm mt-2 bg-transparent border border-primary rounded-none text-xl tracking-wider my-3">
+                            <option value="seller">Seller</option>
+                            <option value="User">User</option>
+            
+                     </select>
+                    </div>
+                    <input className='btn btn-primary text-white  w-full mt-4 text-xl tracking-wider rounded-none mb-2 ' value="Sign Up" type="submit" />
                     {signUpError && <p className='text-red-600'>{signUpError}</p>}
+
+                    
+
+
                 </form>
-                <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
+                <p>Already have an account ?<Link className='text-secondary' to="/login">Please Login</Link></p>
                 <div className="divider">OR</div>
-                <button className='btn btn-outline w-full hover:bg-primary hover:border-none gap-2' onClick={handleGoogleSignIn}><FaGoogle></FaGoogle>CONTINUE WITH GOOGLE</button>
-
-            </div>
-        </div>
+                <button className='btn btn-outline w-full rounded-none border-primary text-xl tracking-wider text-white hover:bg-primary hover:border-none gap-2' onClick={handleGoogleSignIn}><FaGoogle></FaGoogle>CONTINUE WITH GOOGLE</button>
+       </div>
       </div>
-     </div>
-    );
-};
+      <div className='p-5 lg:p-12'></div>
+    </div>
+  )
+}
 
-export default Register;
+export default Register
